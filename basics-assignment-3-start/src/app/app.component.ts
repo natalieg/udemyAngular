95451d02c9dac580e82ea4b37f6edc1829a027b3
Natalie <natalie.gornicki@gmail.com>
2020-04-29 16:37:55 +0200

    update

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -5,5 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  showSecret: boolean = false;
+  clickCounter: number = 0;
+  clickCountArray = [];
+
+  toggleSecret(){
+    let oldStat = this.showSecret;
+    this.showSecret = !oldStat;
+    this.clickCounter = this.clickCounter + 1;
+    this.clickCountArray.push(this.clickCounter);
+    console.log(this.clickCountArray)
+  }
+
+  getStyle(number){
+    return number >= 5  ? '#a6f5ad' : null;
+  }
+
 }
