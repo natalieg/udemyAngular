d5b68cf328d3acd494a3538702ae0538186c0eab
Natalie <natalie.gornicki@gmail.com>
2020-05-04 09:45:43 +0200

    compponent updates

--- recipesProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipesProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Beautiful Test Recipe', 
+    'This is a test, look at all those information!!', 
+    'https://images.pexels.com/photos/1109197/pexels-photo-1109197.jpeg'),
+    new Recipe('PANCAKES OMG', 
+    'This is a test, look at all those information!!', 
+    'https://images.pexels.com/photos/357573/pexels-photo-357573.jpeg'),
+  ];
 
   constructor() { }
 
